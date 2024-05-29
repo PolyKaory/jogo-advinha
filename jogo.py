c57424e74f061d709264f51299b35848a522f4e2
Polyana Suzuki <polyanasuzukiescola@gmail.com>
2024-05-29 13:32:07 -0300

    commit inicial

---
+++ jogo.py
@@ -0,0 +1,7 @@
+import random
+
+def jogo_advinhação():
+    numero_secreto = random.randint(1, 100)
+    tentativas = 0
+
+    
---
+++ teste.py
@@ -0,0 +1,4 @@
+num = int(input("Digite um número"))
+
+if num > 18:
+    print("Maior de idade")
